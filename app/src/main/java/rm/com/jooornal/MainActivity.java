b339512317aab99de9e2c40eeb0ed544d7290e3a
alex <rupta3@gmail.com>
2017-01-16 23:48:33 +0300

    Initial commit

---
+++ app/src/main/java/rm/com/jooornal/MainActivity.java
@@ -0,0 +1,14 @@
+package rm.com.jooornal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
